8307f9829f068dc9e95e69b9483d0de9fdddd14f
Ivan Marquez <ivanmarquez72@aragon.unam.mx>
2022-07-20 14:43:49 -0500

    Modificacion desde la rama prueba

--- PruebaGit2/src/MainGit.java
+++ PruebaGit2/src/MainGit.java
@@ -1,5 +1,6 @@
 public class MainGit {
     public static void main(String[] args) {
         System.out.println("Este sera un nuevo ejemplo desde IntelliJ");
+        System.out.println("Modificacion de la rama ");
     }
 }
